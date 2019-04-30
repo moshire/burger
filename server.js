7d8199b41539057a90ac2c773c67ceee6ec832bf
mohamed shire <m.x.shire876@hotmail.com>
2019-04-30 03:54:35 -0500

    i modified the files

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+//Select and set Handlebars as the engine and "main" as the default layout
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Imports the routes from burgers_controller
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+app.listen(PORT, function () {
+
+    console.log(console.log('App listening on PORT' + PORT));
+});
