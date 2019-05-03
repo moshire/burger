48b7263e3800a933c7c9a8699b8b4fd7b0ea9ae5
mohamed shire <m.x.shire876@hotmail.com>
2019-05-03 07:46:34 -0500

    updated the .env file

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "somali4047669",
+        password: "",
         database: "burgers_db"
     });
 };
